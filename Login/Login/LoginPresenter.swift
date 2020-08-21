4c98bfd0d121a43d40fbd76ef5b52d88c4e3351a
Christophe Bugnon <christophe.bugnon@musicworldmedia.com>
2020-08-21 13:01:12 +0200

    Create LoginPresenter file

---
+++ Login/Login/LoginPresenter.swift
@@ -0,0 +1,18 @@
+//
+//  LoginPresenter.swift
+//  Login
+//
+//  Created by Christophe Bugnon on 21/08/2020.
+//
+
+import Foundation
+
+final class LoginPresenter: LoginUseCaseOutput {
+    func loginSucceeded() {
+        // create welcome view model and pass it to the view controller
+    }
+    
+    func loginFailed() {
+        // create error view model and pass it to the view controller
+    }
+}
